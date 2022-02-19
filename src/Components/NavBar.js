13c81cb8c492472db6bb2f4d6c40d50aef873ded
Martin Facchinello <martinfac@hotmail.com>
2022-02-19 17:16:05 -0300

    Import de Font y aplicación a todao el proyecto, incremento de Height en Logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,16 +3,16 @@ import logo from './Images/Logo Avion.png'
 
 const NavBar = () => {
   return (
-    <nav className="fixed w-full flex justify-between items-center bg-gray-900 px-4">
+    <nav className="fixed w-full flex justify-between items-center bg-blue-800 px-4">
       <div className="flex items-center justify-between h-20">
-        <img className="h-12 mr-4" src={logo} alt="Fachi Travel Design"/>
+        <img className="h-16 mr-4" src={logo} alt="Fachi Travel Design"/>
         <p className="text-white text-3xl font-bold">FACHI TRAVEL DESIGN</p>
       </div>
       <div className="mr-4">
         <ul className="flex justify-between list-none">
-          <li className="text-white mx-8 no-underline"><NavLink to="/" activeClassName="active">Inicio</NavLink></li>
-          <li className="text-white mx-8 no-underline"><NavLink to="/Destinos" activeClassName="active">Destinos</NavLink></li>
-          <li className="text-white mx-8 no-underline"><NavLink to="/Contacto" activeClassName="active">Contacto</NavLink></li>
+          <li className="text-white mx-8 no-underline hover:text-red-500"><NavLink to="/" activeClassName="active">Inicio</NavLink></li>
+          <li className="text-white mx-8 no-underline hover:text-red-500"><NavLink to="/Destinos" activeClassName="active">Destinos</NavLink></li>
+          <li className="text-white mx-8 no-underline hover:text-red-500"><NavLink to="/Contacto" activeClassName="active">Contacto</NavLink></li>
         </ul>
       </div>
     </nav>
